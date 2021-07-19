afdf9f3cd0af2b1aba0a63704339ebfde94b817a
adnanahtas <adnanahtas@gmail.com>
2021-07-19 22:14:00 +0530

    HA electronics page added

--- assets/js/configs.js
+++ assets/js/configs.js
@@ -4,6 +4,12 @@ var configs = [
     name: 'VISHWA AUTO',
     img_src: './images/VishvaUpi.jpeg',
     upi_id: '9923562727@ybl',
-    payment_url: 'upi://pay?pa=9923562727@ybl&pn=VISHWA%20AUTO&mc=0000&mode=02&purpose=00'
+    payment_url: 'upi://pay?pa=9923562727@ybl&pn=VISHWA%20AUTO&mc=0000&mode=02&purpose=00',
+
+    id: 'haelectronics',
+    name: "Demo (Don't Pay)",
+    img_src: './images/VishvaUpi.jpeg',
+    upi_id: 'Customer UPI ID',
+    payment_url: '',
   }
 ]
