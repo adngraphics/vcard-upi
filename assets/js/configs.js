a3f8f82ac1f9677db635a7c6e09134d6a2b9f5c5
adnanahtas <adnanahtas@gmail.com>
2021-07-20 12:35:33 +0530

    shree fabrication page added

--- assets/js/configs.js
+++ assets/js/configs.js
@@ -5,11 +5,18 @@ var configs = [
     img_src: './images/VishvaUpi.jpeg',
     upi_id: '9923562727@ybl',
     payment_url: 'upi://pay?pa=9923562727@ybl&pn=VISHWA%20AUTO&mc=0000&mode=02&purpose=00',
-
+  },{
     id: 'haelectronics',
     name: "Demo (Don't Pay)",
     img_src: './images/VishvaUpi.jpeg',
     upi_id: 'Customer UPI ID',
     payment_url: '',
+  },{
+    id: 'shreefabrication',
+    name: "Rajkumar Panchal",
+    img_src: './images/rajupanchalUpi.jpeg',
+    upi_id: '9096483660@ybl',
+    payment_url: 'upi://pay?pa=9096483660@ybl&pn=Rajkumar%20vaijenath%20panchal&mc=0000&mode=02&purpose=00'
+    
   }
 ]
