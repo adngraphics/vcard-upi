de58ca3ba334487d8011da0688b99913c8530c77
Aziz Ahtas <azizahtas@gmail.com>
2021-07-20 11:37:31 +0530

    Fixed Config Bug

--- assets/js/configs.js
+++ assets/js/configs.js
@@ -5,7 +5,7 @@ var configs = [
     img_src: './images/VishvaUpi.jpeg',
     upi_id: '9923562727@ybl',
     payment_url: 'upi://pay?pa=9923562727@ybl&pn=VISHWA%20AUTO&mc=0000&mode=02&purpose=00',
-
+  },{
     id: 'haelectronics',
     name: "Demo (Don't Pay)",
     img_src: './images/VishvaUpi.jpeg',
