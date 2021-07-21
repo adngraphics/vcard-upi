6fe659d6848d108f7a7841a87dc192cee4dfe295
adnanahtas <adnanahtas@gmail.com>
2021-07-21 12:26:11 +0530

    ahtastraders page and accurate page added but qr code remaining

--- assets/js/configs.js
+++ assets/js/configs.js
@@ -17,6 +17,17 @@ var configs = [
     img_src: './images/rajupanchalUpi.jpeg',
     upi_id: '9096483660@ybl',
     payment_url: 'upi://pay?pa=9096483660@ybl&pn=Rajkumar%20vaijenath%20panchal&mc=0000&mode=02&purpose=00'
-    
+  },{
+    id: 'accurateeng',
+    name: "Vinod Kakade",
+    img_src: '',
+    upi_id: '',
+    payment_url: ''
+  },{
+    id: 'ahtastraders',
+    name: "Khozema Ahtas",
+    img_src: './images/ahtastradersUpi.jpeg',
+    upi_id: '9096483660@ybl',
+    payment_url: 'upi://pay?pa=9096483660@ybl&pn=Rajkumar%20vaijenath%20panchal&mc=0000&mode=02&purpose=00'
   }
 ]
